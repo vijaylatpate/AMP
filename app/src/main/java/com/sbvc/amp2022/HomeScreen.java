b40f9c6f5f8f08ece84ad464abc16610d128662b
programmerprofessor <programmerprofessor@gmail.com>
2022-02-17 22:37:06 +0530

    Basic Layout Of Application

--- app/src/main/java/com/sbvc/amp2022/HomeScreen.java
+++ app/src/main/java/com/sbvc/amp2022/HomeScreen.java
@@ -4,10 +4,17 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class HomeScreen extends AppCompatActivity {
+public class HomeScreen extends AppCompatActivity implements View.OnClickListener {
     AlertDialog.Builder builder;
+
+    Button btnPractical1, btnPractical2, btnPractical3, btnPractical4, btnPractical5;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,5 +45,21 @@ public class HomeScreen extends AppCompatActivity {
         //Setting the title manually
         alert.setTitle(R.string.app_name);
         alert.show();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnPractical_1:
+                Toast.makeText(this, "Learn Android Architecture & Components", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btnPractical_2:
+                Toast.makeText(this, "App Developed On Android Studio", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btnPractical_3:
+                startActivity(new Intent(getApplicationContext(), Practical_3.class));
+                break;
+
+        }
     }
 }
---
+++ app/src/main/java/com/sbvc/amp2022/Practical_3.java
@@ -0,0 +1,38 @@
+package com.sbvc.amp2022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Practical_3 extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnPractical3_1, btnPractical3_2, btnPractical3_3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practical3);
+
+        btnPractical3_1 = findViewById(R.id.btnPractical_3_1);
+        btnPractical3_2 = findViewById(R.id.btnPractical_3_2);
+        btnPractical3_3 = findViewById(R.id.btnPractical_3_3);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnPractical_3_1:
+                startActivity(new Intent(getApplicationContext(), Practical_3_1.class));
+                break;
+            case R.id.btnPractical_3_2:
+                startActivity(new Intent(getApplicationContext(), Practical_3_2.class));
+                break;
+            case R.id.btnPractical_3_3:
+                startActivity(new Intent(getApplicationContext(), Practical_3_3.class));
+                break;
+        }
+    }
+}
